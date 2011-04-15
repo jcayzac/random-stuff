802227b2ae6a26a922f40555b08ffd03126a833b
Julien Cayzac <julien.cayzac@gmail.com>
2011-04-15 18:07:32 +0900

    md2pdf + awesome edition

--- __scripts__/text/md2pdf/md2pdf.js
+++ __scripts__/text/md2pdf/md2pdf.js
@@ -19,7 +19,9 @@ process.stdin.on('data', function(text) { md.push(text); });
 process.stdin.on('end', function() {
 	var child = spawn("htmldoc", ['-t','pdf14','--color','--book','--no-jpeg','--header','.tc','--footer','..1','-']);
 	child.stdout.on('data', function(x) { process.stdout.write(x); });
-	child.stdin.write(converter.makeHtml(md.join('')));
+	var html=converter.makeHtml(md.join(''));
+	html = html.replace(/<img /, '<img height="150pt" ');
+	child.stdin.write(html);
 	child.stdin.end();
 });
 
