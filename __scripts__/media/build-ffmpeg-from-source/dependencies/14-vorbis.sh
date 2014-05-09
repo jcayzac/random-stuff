1d81490f2093a0d95bf30c42257056d90249a8d5
Julien Cayzac <julien.cayzac@mail.rakuten.com>
2014-05-09 09:59:14 +0900

    Update VORBIS

--- __scripts__/media/build-ffmpeg-from-source/dependencies/14-vorbis.sh
+++ __scripts__/media/build-ffmpeg-from-source/dependencies/14-vorbis.sh
@@ -1,9 +1,9 @@
-wget http://downloads.xiph.org/releases/vorbis/libvorbis-1.2.3.tar.gz
-tar xf libvorbis-1.2.3.tar.gz
-cd libvorbis-1.2.3
+wget http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.4.tar.gz
+tar xf libvorbis-1.3.4.tar.gz
+cd libvorbis-1.3.4
 ./configure --prefix="$PREFIX"
 make -j4
 make -j4 install
 cd ..
-rm -rf libvorbis-1.2.3*
+rm -rf libvorbis-1.3.4*
 
