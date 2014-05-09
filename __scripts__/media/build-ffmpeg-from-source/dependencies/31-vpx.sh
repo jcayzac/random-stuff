df676936f284cee1e6e8a9b68b07c89105af05e3
Julien Cayzac <julien.cayzac@mail.rakuten.com>
2014-05-09 10:08:53 +0900

    Update VPX

--- __scripts__/media/build-ffmpeg-from-source/dependencies/31-vpx.sh
+++ __scripts__/media/build-ffmpeg-from-source/dependencies/31-vpx.sh
@@ -1,9 +1,8 @@
-wget http://webm.googlecode.com/files/libvpx-v1.1.0.tar.bz2
-tar xf libvpx-v1.1.0.tar.bz2
-cd libvpx-v1.1.0
-./configure --prefix="$PREFIX" --cpu=core2 --enable-vp8 --enable-postproc --disable-install-docs --disable-examples
+git clone https://chromium.googlesource.com/webm/libvpx
+cd libvpx
+./configure --prefix="$PREFIX" --cpu=${CPU:-core2} --enable-vp8 --enable-postproc --disable-install-docs --disable-examples
 make -j4
 make -j4 install
 cd ..
-rm -rf libvpx-*
+rm -rf libvpx
 
