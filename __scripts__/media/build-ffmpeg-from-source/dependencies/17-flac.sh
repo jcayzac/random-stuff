a40bb5e115d1b7ac4dbf2accaed6220501009536
Julien Cayzac <julien.cayzac@mail.rakuten.com>
2014-05-09 10:00:29 +0900

    Update FLAC

--- __scripts__/media/build-ffmpeg-from-source/dependencies/17-flac.sh
+++ __scripts__/media/build-ffmpeg-from-source/dependencies/17-flac.sh
@@ -1,7 +1,7 @@
-wget http://downloads.xiph.org/releases/flac/flac-1.2.1.tar.gz
-tar xf flac-1.2.1.tar.gz
-cd flac-1.2.1
-# asm sources seem broken
+wget http://downloads.xiph.org/releases/flac/flac-1.3.0.tar.gz
+tar xf flac-1.3.0.tar.gz
+cd flac-1.3.0
+# FIXME: asm sources seem broken
 ./configure --prefix="$PREFIX" --disable-doxygen-docs --disable-asm-optimizations
 make -j4
 make -j4 install
