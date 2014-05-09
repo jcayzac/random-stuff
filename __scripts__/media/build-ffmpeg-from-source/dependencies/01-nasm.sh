af08e647048b853f3fb5472a6c6824d71dba8ced
Julien Cayzac <julien.cayzac@mail.rakuten.com>
2014-05-09 09:53:50 +0900

    Update NASM

--- __scripts__/media/build-ffmpeg-from-source/dependencies/01-nasm.sh
+++ __scripts__/media/build-ffmpeg-from-source/dependencies/01-nasm.sh
@@ -1,8 +1,8 @@
-wget http://www.nasm.us/pub/nasm/releasebuilds/2.10.05/nasm-2.10.05.tar.xz
-tar xf nasm-2.10.05.tar.xz
-cd nasm-2.10.05
+wget http://www.nasm.us/pub/nasm/releasebuilds/2.11.02/nasm-2.11.02.tar.xz
+tar xf nasm-2.11.02.tar.xz
+cd nasm-2.11.02
 ./configure --prefix="$PREFIX"
 make -j4 install
 cd ..
-rm -rf nasm-2.10.05*
+rm -rf nasm-2.11.02*
 
