a2e8316641b734edaf1d16d35454d1c10ed85602
Julien Cayzac <julien.cayzac@mail.rakuten.com>
2014-05-09 09:55:47 +0900

    Update PNG

--- __scripts__/media/build-ffmpeg-from-source/dependencies/04-png.sh
+++ __scripts__/media/build-ffmpeg-from-source/dependencies/04-png.sh
@@ -2,12 +2,12 @@ git clone git://libpng.git.sourceforge.net/gitroot/libpng/libpng
 cd libpng
 git fetch --tags
 # Builds 1.2
-git checkout v1.2.42
+git checkout v1.2.51
 sed -i '' -E 's/libtoolize/glibtoolize/g' autogen.sh
 ./autogen.sh
 ./configure --prefix="$PREFIX" --enable-static --disable-shared
 make -j4 install
-# Builds 1.5
+# Builds 1.6
 git reset --hard
 git clean -fdx
 git checkout master
