2793201e1d93de5fa6672a5da02aa1047eb6ff27
Julien Cayzac <julien.cayzac@mail.rakuten.com>
2014-05-09 10:02:29 +0900

    Update OPUS

--- __scripts__/media/build-ffmpeg-from-source/dependencies/19-opus.sh
+++ __scripts__/media/build-ffmpeg-from-source/dependencies/19-opus.sh
@@ -1,6 +1,6 @@
-wget http://downloads.xiph.org/releases/opus/opus-1.0.1.tar.gz
-tar xf opus-1.0.1.tar.gz
-cd opus-1.0.1
+wget http://downloads.xiph.org/releases/opus/opus-1.1.tar.gz
+tar xf opus-1.1.tar.gz
+cd opus-1.1
 ./configure --prefix="$PREFIX" --enable-custom-modes --enable-float-approx --disable-doc
 make -j4
 make -j4 install
